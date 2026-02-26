b9855b0d831d59864438e3b0d332e006d45e05d9
c0pson <pc312199@student.polsl.pl>
2026-02-26 17:46:42 +0100

    Refactor STATE class to improve organization and add new state methods

--- src/const.py
+++ src/const.py
@@ -1,18 +1,36 @@
 from enum import IntEnum, StrEnum
+import random
 
 class SIZE(IntEnum):
     TITLE_BAR = 32
     SCREEN_WIDTH = 300
     SCREEN_HEIGHT = 300
+    CAT = 256
 
 class STATE(StrEnum):
-    IDLE_1     = "idle_1"
-    IDLE_2     = "idle_2"
-    CLEANING_1 = "cleaning_1"
-    CLEANING_2 = "cleaning_2"
-    RUNNING_1  = "running_1"
+    IDLE_1     = "idle_1"       # 
+    IDLE_2     = "idle_2"       # IDLE
+    CLEANING_1 = "cleaning_1"   # 
+    CLEANING_2 = "cleaning_2"   # 
+    RUNNING_1  = "running_1"    
     RUNNING_2  = "running_2"
-    SLEEPING   = "sleeping"
-    WALKING    = "walking"
-    JUMPING    = "jumping"
-    HISSING    = "hissing"
+    SLEEPING   = "sleeping"     # IDLE
+    PLAYING    = "playing"
+    JUMPING    = "jumping"      
+    HISSING    = "hissing"      # INTERACTION WITH MOUSE RANDOMLY
+
+    @staticmethod
+    def random():
+        return random.choice(list(STATE))
+
+    @staticmethod
+    def idle_states():
+        return [STATE.PLAYING, STATE.IDLE_1, STATE.IDLE_2, STATE.CLEANING_1, STATE.CLEANING_2, STATE.SLEEPING]
+    
+    @staticmethod
+    def fast_states():
+        return [STATE.RUNNING_1, STATE.RUNNING_2]
+
+    @staticmethod
+    def walking_states():
+        return [STATE.JUMPING]
