089001fed989ddd10cb4e634e882df852eb49695
c0pson <pc312199@student.polsl.pl>
2026-02-26 17:46:59 +0100

    Refactor App class to integrate STATE management and improve window movement logic

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 import random
 
 from utils import get_working_area
-from const import SIZE
+from const import SIZE, STATE
 from pet_sprite import Cat
 
 class App(ctk.CTk):
@@ -16,10 +16,16 @@ class App(ctk.CTk):
         self.geometry(
             f"{SIZE.SCREEN_WIDTH}x{SIZE.SCREEN_HEIGHT}+{self.window_width_pos}+{self.window_height_pos}"
         )
+        self.configure(fg_color="#00ff00")
+        self.wm_attributes('-topmost', True)
+        self.wm_attributes('-transparentcolor', '#00ff00')
+        self.lift()
         self.overrideredirect(True)
-        self.attributes("-topmost", True)
         self.cat = Cat(self)
         self.cat.pack(side=ctk.BOTTOM)
+        self.idle_states: list[STATE] = STATE.idle_states()
+        self.fast_states: list[STATE] = STATE.fast_states()
+        self.walking_states: list[STATE] = STATE.walking_states()
         self.after(100, self.move_window)
 
     def move_window(self) -> None:
@@ -27,7 +33,13 @@ class App(ctk.CTk):
             self.velocity *= -1
         if self.window_width_pos > self.max_x_position or self.window_width_pos < -90:
             self.velocity *= -1
-        self.window_width_pos += self.velocity
+        if self.cat.state in self.walking_states:
+            speed = 1
+        elif self.cat.state in self.idle_states or self.cat.state == STATE.HISSING:
+            speed = 0
+        else:
+            speed = 2
+        self.window_width_pos += self.velocity * speed
         self.geometry(
             f"{SIZE.SCREEN_WIDTH}x{SIZE.SCREEN_HEIGHT}+{self.window_width_pos}+{self.window_height_pos}"
         )
